280f6b3410d36626912baa61fc2a0e1b4e96abfc
Michael Deutsch <michaeldeutsch1@gmail.com>
2020-02-05 11:22:24 +0200

    fixing knapsack

--- knapsack.py
+++ knapsack.py
@@ -1,11 +1,20 @@
 from pprint import pprint
 import numpy as np
 
-w = [23, 26, 20, 18, 32, 27, 29, 26, 30, 27]
-v = [505, 352, 458, 220, 354, 414, 498, 545, 473, 543]
+
+# Example from https://en.wikipedia.org/wiki/Knapsack_problem
+# capacity = 67
+weights = [23, 26, 20, 18, 32, 27, 29, 26, 30, 27]
+values = [505, 352, 458, 220, 354, 414, 498, 545, 473, 543]
 
 test_weight = [1, 5, 3, 4]
 test_value = [15, 10, 9, 5]
+
+guru_w = [3, 4, 5, 9, 4]
+guru_v = [3, 4, 4, 10, 4]
+
+guru2_w = [12, 2, 1, 1, 4]
+guru2_v = [4, 2, 1, 2, 10]
 
 def solve(weight_list, value_list, capacity):
     assert(len(weight_list) == len(value_list))
@@ -13,19 +22,25 @@ def solve(weight_list, value_list, capacity):
     v = [0] + value_list
     n = len(w)
 
-    table = np.zeros((n, capacity), dtype=np.int16)
+    table = np.zeros((n, capacity +1 ), dtype=np.int16)
     
     for i in range(1, n):
-        for capacity_remaining in range(capacity):
+        for capacity_remaining in range(capacity + 1):
             if w[i] <= capacity_remaining:
                 table[i, capacity_remaining] = max(
                     table[i-1, capacity_remaining],     # don't take it
-                    v[i] + table[i, capacity_remaining - w[i]] # take it
+                    v[i] + table[i -1, capacity_remaining - w[i]] # take it
                 )
             else:
                 table[i, capacity_remaining] = table[i-1, capacity_remaining]
+    print(table)
+    take = []
+    j = capacity
+    for i in range(n-1, 0, -1):
+        if table[i, j] != table[i-1, j]:
+            take.append(i)
+            j -= w[i]
 
-    return table
+    return table[-1, -1], take
 
-results = solve(test_weight, test_value, 8)
-print(results)
+print(solve(weights, values, 67))
